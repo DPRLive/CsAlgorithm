93b3044a83702c51ac728a0b31f1e3d799ebd5ff
DPRLive <chosy0107@naver.com>
2020-08-08 23:35:42 +0900

    1day

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CsAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            board.Initialize(25);
+            board.Render();
+        }
+    }
+}
